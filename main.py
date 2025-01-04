18f3a37a83295324feef6511810a09834e907aff
CSEAryan <dipuaryan1111@gmail.com>
2025-01-04 11:11:36 +0545

    model training done

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from chicken_diseases_classification.pipeline.stage_03_training import ModelTrainingPipeline
 from src.chicken_diseases_classification import logger
 from src.chicken_diseases_classification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.chicken_diseases_classification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
@@ -19,6 +20,18 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     prepare_base_model = PrepareBaseModelTrainingPipeline()
     prepare_base_model.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Training"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_trainer = ModelTrainingPipeline()
+    model_trainer.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
